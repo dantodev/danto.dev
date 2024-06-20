a017062b437d0fa12828e7d7de966aa954f4ff82
Daniel Kahl <me@danto.dev>
2024-06-21 00:54:50 +0200

    added lint-staged and eslint-plugin-json

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,13 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import json from "eslint-plugin-json";
 
 export default withNuxt([
   eslintPluginPrettierRecommended,
+  {
+    files: ["**/*.json"],
+    ...json.configs["recommended"]
+  },
   {
     rules: {
       "prettier/prettier": "warn",
